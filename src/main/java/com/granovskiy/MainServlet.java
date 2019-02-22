dbbdf6120f482481adcb37172bad17a396e9fee3
Sviatoslav <s.granovskiy@gmail.com>
2019-02-22 14:50:00 +0200

    GET method, Hello from Servlet

--- src/main/java/com/granovskiy/MainServlet.java
+++ src/main/java/com/granovskiy/MainServlet.java
@@ -8,8 +8,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
+    /*  method doGET is used to get the info from the (local) server;
+        the arguments are: HttpServletRequest req, HttpServletResponse resp;
+        the method throws exceptions: ServletException, IOException
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //  PrintWriter is used to create an html page with the words "Hello from Servlet!"
         PrintWriter writer = resp.getWriter();
         writer.println("<html>");
         writer.println("<head>");
